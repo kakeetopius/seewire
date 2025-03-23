1d31a04b5eefd0bacb5e1988041c941408249c95
kakeetopius <pius.kakeeto@strathmore.edu>
2025-03-23 22:30:53 +0300

    Refactoring

--- Includes/main.h
+++ Includes/main.h
@@ -9,12 +9,12 @@
 #define UDP_HEADER_LEN (sizeof(struct udphdr)) 
 
 /*---------------------FLAGS FOR INPUTTED OPTIONS----------------------------------*/
-#define A_FLAG 0x01
-#define P_FLAG 0x02
-#define F_FLAG 0x04
+#define A_FLAG 0x01  //00000001
+#define P_FLAG 0x02  //00000010
+#define F_FLAG 0x04  //00000100
 
 
-/*---------------------FUNCTION DECLARATIONS FOR MAIN FUNCTION---------------------------------------*/
+/*---------------------FUNCTION DECLARATIONS FOR MAIN FILE---------------------------------------*/
 void callback(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes);
 void handle_ethernet(const u_char* packet);
 void handle_ip4(const u_char* packet);
